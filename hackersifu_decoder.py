faa820ec17431898a7c6bcb746cc0b0f6bb15b9e
hackersifu <jwmckiddy31@gmail.com>
2019-10-27 14:15:43 -0700

    Update hackersifu_decoder.py
    Fixed some syntax

--- hackersifu_decoder.py
+++ hackersifu_decoder.py
@@ -6,7 +6,8 @@ import time
 print("Hackersifu Base64 Super Decoder Ring")
 time.sleep(1)
 secret = raw_input("Base64 encoded value? ")
-
-base64.b64decode(secret)
-print("Thanks!")
+print("Coming right up...")
+time.sleep(1)
+print("Here is the value: " + base64.b64decode(secret))
+print("The value is above this line. Thanks!")
 sys.exit(0)
