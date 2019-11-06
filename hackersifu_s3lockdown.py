9a4295e0c2b27fcb37c54a249e885efabbefdacb
hackersifu <jwmckiddy31@gmail.com>
2019-11-05 22:34:44 -0800

    Initial Commit

---
+++ hackersifu_s3lockdown.py
@@ -0,0 +1,27 @@
+import boto3
+import json
+import sys
+
+def lambda_handler(event, context):
+    # The following code relies on the CloudWatch trigger JSON configuration contained in the trigger.json file.
+    s3 = boto3.resource('s3')
+    client = boto3.client('s3')
+    for bucket in s3.buckets.all():
+      print(bucket.name)
+    for bucket in s3.buckets.all():
+      response = client.put_public_access_block(
+          Bucket=bucket.name,
+          PublicAccessBlockConfiguration={
+              'BlockPublicAcls': True,
+              'IgnorePublicAcls': True,
+              'BlockPublicPolicy': True,
+              'RestrictPublicBuckets': True
+    }
+)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Your buckets are secure from the hacks!')
+    
+    }
+    sys.exit()
