f9e7a10632a03600623f4e0482e3b11d88cf0187
hackersifu <jwmckiddy31@gmail.com>
2019-11-09 09:54:29 -0800

    Update hackersifu_s3lockdown.py

--- hackersifu_s3lockdown.py
+++ hackersifu_s3lockdown.py
@@ -3,7 +3,7 @@ import json
 import sys
 
 def lambda_handler(event, context):
-    # The following code can be invoked by a trigger or manually.
+    # The following code is designed to be invoked through a CloudWatch rule.
     s3 = boto3.resource('s3')
     client = boto3.client('s3')
     for bucket in s3.buckets.all():
