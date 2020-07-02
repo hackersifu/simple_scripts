4af84b619b14c9e75c8356474f4ede1913368a50
hackersifu <>
2020-07-02 15:12:54 -0700

    Test Over

--- prowler_script.py
+++ prowler_script.py
@@ -18,5 +18,4 @@ print("The prerequisites should be configured. Moving on to running Prowler, and
 time.sleep(1)
 os.system("cd prowler && sudo ./prowler -M csv")
 print("Check the prowler directory to find the csv file. Thanks!")
-#Test 222
 sys.exit()
